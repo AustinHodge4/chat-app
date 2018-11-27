55fe1cc63f87561dfca1c192affc869a9c048a25
Austin Hodge <hodgeaj4@gmail.com>
2018-11-27 00:20:10 -0500

    Root redirect

--- app/chat/urls.py
+++ app/chat/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.shortcuts import redirect
 from chat.views import index, loginuser, logoutuser, MessageView, ChannelView, UserView
 
 urlpatterns = [
+    path('', lambda request: redirect('home/', permanent=False)),
     path('home/', loginuser),
     path('api/', index),
     path('api/logout/',logoutuser),
