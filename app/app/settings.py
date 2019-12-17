8ff1b319610df2d255bdb1ffb3e7479eaf23cbd6
Austin <austin@Austins-MacBook-Pro.local>
2019-12-17 09:40:01 -0500

    possible fix for favicon missing

--- app/app/settings.py
+++ app/app/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'frontend/static/'),
